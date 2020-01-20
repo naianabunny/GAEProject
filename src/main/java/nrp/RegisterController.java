91f530aa7c160cadd1fbec4aa0078a2520a651d8
s200012700256 <s200012700256@MUM00127MR12.NIITANDHERI.COM>
2020-01-20 17:00:35 +0530

    changes

--- src/main/java/nrp/RegisterController.java
+++ src/main/java/nrp/RegisterController.java
@@ -27,7 +27,7 @@ public class RegisterController extends HttpServlet
 		try 
 		{
 			Connection con=DriverManager.getConnection("jdbc:mysql://35.255.40.133:3306/db","root","root");
-			PreparedStatement ps=con.prepareStatement("insert into users values(?,?,?)");
+			PreparedStatement ps=con.prepareStatement("insert into users (username,email,password) values(?,?,?)");
 			ps.setString(1, u.getUsername());
 			ps.setString(2, u.getEmail());
 			ps.setString(3, u.getPassword());
